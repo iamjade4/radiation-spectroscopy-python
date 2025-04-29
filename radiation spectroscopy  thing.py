7f06adf803f2ae691beaf00e6824a45926b3fecf
K <brandon.bigham12@gmail.com>
2025-04-28 21:07:05 -0700

    comment cleanup

--- radiation spectroscopy  thing.py
+++ radiation spectroscopy  thing.py
@@ -14,7 +14,6 @@ class IDetector:
     def detects(self, particle: IParticle):
         pass
 
-#c = const.c
 photons = []
 detected = 0
 #2darray containing the information about all of the photons. Unsure as to whether I should even be using classes or if I should directly append particles to the array
@@ -34,12 +33,6 @@ class photon(IParticle):
         self.z = z
         self.t = t
         self._dx, self._dy, self._dz = self._calculate_direction()
-    
-    # def energy(self):
-    #     E = math.sqrt(self.px**2 + self.py**2 + self.pz**2)
-    #     return E
-    #def position(self):
-    #    return self.px, self.py, self.pz, self.theta, self.phi, self.x, self.y, self.z, self.t
 
     def _calculate_direction(self):
         if self.pz < 0:
@@ -61,27 +54,6 @@ class photon(IParticle):
     def energy(self):
         return math.sqrt(self.px**2 + self.py**2 + self.pz**2)
 
-        #Normalise Z
-        #dt*c*sin(phi) = 1 --> dt = 1/(c*sin(phi))
-        # dt = 1/(c*np.sin(phi)) #Normalising my vector and then flipping the sign if negative (it only extends into one direction from the origin, not in both directions, so the sign actually matters)
-        # if pz<0:                #This is a lot quicker, the inefficient part is the Detection method
-        #     dz = -1
-        #     dx = -np.cos(theta)*np.cos(phi)/np.sin(phi)
-        #     dy = -np.sin(theta)*np.cos(phi)/np.sin(phi)
-        # else:
-        #     dz = 1
-        #     dx = np.cos(theta)*np.cos(phi)/np.sin(phi)
-        #     dy = np.sin(theta)*np.cos(phi)/np.sin(phi)
-
-
-        # for i in range(1000):
-        #     position = [i*dx+x,i*dy+y,i*dz+z]
-        #     arraytrace.append(position)
-        # vector = [dx, dy, dz]
-        # origin = sy.Point3D(x, y, z)
-        # path = sy.Ray(origin, direction_ratio=vector)
-        # return path
-        return x, y, z, dx, dy, dz
 
 class NaITl(IDetector):
     def __init__(self, x, y, z, X, Y, Z):
@@ -94,28 +66,6 @@ class NaITl(IDetector):
         self.Y = Y
         self.Z = Z
         self._bounds = self._calculate_bounds()
-    # def planes(self):  #OBSOLETE
-    #     #Going to try using planes to define the geometry of the detector
-    #     x = self.x
-    #     y = self.y
-    #     z = self.z
-    #     X = self.X
-    #     Y = self.Y
-    #     Z = self.Z#This only works for a cuboidal structure
-    #     vrt1 = (x, y, z)
-    #     vrt2 = (x + X, y, z)
-    #     vrt3 = (x, y + Y, z)
-    #     vrt4 = (x + X, y + Y, z)
-    #     vrt5 = (x, y, z+Z)
-    #     vrt6 = (x + X, y, z+Z)
-    #     vrt7 = (x, y + Y, z+Z)
-    #     vrt8 = (x + X, y + Y, z+Z)                          #Using slab method
-    #     xyc = sy.Plane(vrt1, vrt2, vrt3)#XY plane close
-    #     xyf = sy.Plane(vrt5, vrt6, vrt7)#XY plane far
-    #     xzc = sy.Plane(vrt1, vrt2, vrt6)#XZ plane close
-    #     xzf = sy.Plane(vrt3, vrt8, vrt7)#XZ plane far
-    #     yzc = sy.Plane(vrt1, vrt3, vrt5)#ZY plane close
-    #     yzf = sy.Plane(vrt2, vrt4, vrt6)#ZY plane far
 
     def _calculate_bounds(self):
         return (
@@ -124,16 +74,6 @@ class NaITl(IDetector):
             self.y + self.Y,
             self.z + self.Z
         )
-
-    # #     return xyc, xyf, xzc, xzf, yzc, yzf
-    # def position(self):
-    #     xc = self.x
-    #     yc = self.y
-    #     zc = self.z
-    #     xf = self.X + xc
-    #     yf = self.Y + yc
-    #     zf = self.Z + zc
-    #     return xc, yc, zc, xf, yf, zf
 
     def detects(self, particle: IParticle):
         x0, y0, z0 = particle.get_origin()
@@ -156,35 +96,8 @@ class NaITl(IDetector):
         return tclose <= tfar
 
 
-# def Detection(detector, particle):#The "detector" argument takes a list of planes for a detector
-#     #planes = detector.planes()
-#     bounds = detector.position()
-#     ray = particle.raytrace()
-#     distc = []
-#     distf = []
-#     for i in range(3):
-#         distc.append((bounds[i]-ray[i])/ray[i+3])
-#         distf.append((bounds[i+3]-ray[i])/ray[i+3])
-#         #intr = planes[i].intersection(particle.raytrace()) #this is p(t)
-#     tclose = max(distc)
-#     tfar = min(distf)
-#     if tclose <= tfar:
-#         detection =1
-#         #print(intr)
-#     # for i in range(len(particle.raytrace())):
-#     #     if detector.position()[0] < particle.raytrace()[i][0] < detector.position()[0] + detector.position()[3] and detector.position()[1] < particle.raytrace()[i][1] < detector.position()[1] + detector.position()[4] and detector.position()[2] < particle.raytrace()[i][2] < detector.position()[2] + detector.position()[5]: 
-#     #          #cross checking each term in the array arraytrace against the detector's coordinates. This is incredibly inefficient
-#     #          detection = 1
-#     #          break
-#     #     else:
-#     #         detection = 0
-#     else:
-#         detection = 0
-#     return detection
-
 #Creating the detector once to avoid creating it 1000 times
 detector = NaITl(100, 100, 0, 300, 300, 300)
-#planes = detector.planes()
 
 for i in range(100000):
     #Randomly generated angles however, it will be generating a duplicate for 0 & 2pi, I could find a solution for this (go up to 2pi-1x10^-16 for flt16)
